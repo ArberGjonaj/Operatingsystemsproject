9666acdef440eddd143d0f17d6496b765e91dab6
Arber Gjonaj <agjonaj@ikub.local>
2021-01-10 00:51:50 +0100

    finished the graph needs some more working one regarding one button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ZingChart from 'zingchart-react';
 import ProcessComponent from "./Components/processComponent";
 import "./App.css";
 
@@ -6,10 +7,55 @@ class App extends React.Component {
   state = {
     processData: [],
     students: [
-      { id: 1, name: "Arber", age: 21 },
+      { id: 1, name: "Arber", age: 18 },
       { id: 2, name: "Elidor", age: 19 },
       { id: 3, name: "Ulger", age: 16 },
     ],
+    config: {
+      type: 'bar',
+      "scale-y": {
+        "line-color": "#000",
+        "item": {
+          "font-color": "#000"
+        },
+        "values": "0:20:1",
+        "guide": {
+          "visible": true
+        },
+        "label": {
+          "text": "Seconds",
+          "font-family": "arial",
+          "bold": true,
+          "font-size": "14px",
+          "font-color": "#000",
+        },
+      },
+      "series": [{
+        "values": [
+          18.,
+          19,
+          16
+        ],
+        "alpha": 0.95,
+        "borderRadiusTopLeft": 0.21,
+        "background-color": "#000",
+        "text": "Apple",
+      }],
+      "scaleX": {
+        "values": [//ketu zevendeso proess names
+          "Arber",
+          "Elidor",
+          "Ulger",
+        ],
+      series: [{
+        values: [
+          2,
+         2,
+          2
+        ]
+      }]
+    }
+  }
   };
   renderTableData() {
     return this.state.students.map((student, index) => {
@@ -33,6 +79,7 @@ class App extends React.Component {
             {this.renderTableData()}
           </tbody>
         </table>
+        <ZingChart data={this.state.config}/>
       </div>
     );
   }
